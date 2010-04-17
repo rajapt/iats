4df1a6bfc96a14f827a2b8d76f71a9cc936701b1
reader02 <reader02@8a4b992a-2f3e-11de-8e0c-53a27eea117e>
2010-04-17 23:52:54 +0000

    20100417  first version

---
+++ sl/atl1c/loadrelease.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+cd build
+cd Release
+echo "file attribute adjusting for  AtherosL1cEthernet.kext"
+
+sudo rm -rf /System/Library/Extensions.mkext
+sudo rm -rf /System/Library/Extensions.kextcache
+
+sudo chown -R root:wheel AtherosL1cEthernet.kext
+sudo find AtherosL1cEthernet.kext -type d -exec chmod 0755 {} \;
+sudo find AtherosL1cEthernet.kext -type f -exec chmod 0644 {} \; 
+
+sudo kextload -t AtherosL1cEthernet.kext
+
+
---
+++ sl/atl1c/unloadrelease.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+USER=`whoami`
+cd build
+cd Release
+echo "file attribute adjusting for  AtherosL1cEthernet.kext"
+sudo chown -R root:wheel AtherosL1cEthernet.kext
+sudo find AtherosL1cEthernet.kext -type d -exec chmod 0755 {} \;
+sudo find AtherosL1cEthernet.kext -type f -exec chmod 0644 {} \; 
+
+sudo kextunload AtherosL1cEthernet.kext
+
+echo "restore file attributes..."
+sudo chown -R $USER AtherosL1cEthernet.kext
+
